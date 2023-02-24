f21c789651e5dffc980ec68b193812e72965b84c
JungIn <lego0520lwy@icloud.com>
2023-02-25 00:58:49 +0900

    simple Design

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -2,9 +2,9 @@
     <div>
         Portfolio
         <Flicking :options="{ align: 'prev', circular: true }" @move-end="onMoveEnd">
-            <div class="panel">1</div>
-            <div class="panel">2</div>
-            <div class="panel">3</div>
+            <div v-for="(member, i) in members" :index="i" :key="i">
+                <div class="card"><span class="name">{{ member.name }}</span></div>
+            </div>
         </Flicking>
     </div>
 </template>
@@ -13,6 +13,26 @@
 import { Flicking } from '@egjs/vue-flicking'
 export default {
     name: 'PortfolioPage',
+    data: function(){
+        return {
+            members: [
+                {name: 'Jungin Kim', color: ''},
+                {name: 'Yein Yoon', color: ''},
+                {name: 'Seokbeom Jang', color: ''},
+                {name: 'Jungwon Kim', color: ''},
+                {name: 'Sejin Lee', color: ''},
+                {name: 'Jiseok Lee', color: ''},
+                {name: 'Jeong Heo', color: ''},
+                {name: 'Bongsik Kwon', color: ''},
+                {name: 'Hyuktae Kwon', color: ''},
+                {name: 'Junseok Park', color: ''},
+                {name: 'Yura Park', color: ''},
+                {name: 'Jinho Kim', color: ''},
+                {name: 'Wonkyu Yoo', color: ''},
+                {name: 'Seonmi Kim', color: ''},
+                {name: 'Jinhyeok Ryu', color: ''},]
+        }
+    },
     components: {
         Flicking: Flicking
     }
@@ -24,5 +44,25 @@ export default {
     padding: 0;
     margin: 0;
 }
-
+.card {
+    position: relative;
+    width: 360px;
+    height: 200px;
+    background-color: gray;
+    margin-left: 30px;
+    text-align: center;
+    border-radius: 8px;
+}
+.card .name {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    width: 100%;
+    transform: translate(-50%, -50%);
+    font-family: 'barcode';
+    font-size: 50px;
+}
+.card:hover {
+    background-color: orange;
+}
 </style>
