f24ffefcef6b010e4f7233ecc9e970c698f4307e
JungIn <lego0520lwy@icloud.com>
2023-02-24 23:30:39 +0900

    commit...

---
+++ pages/wiki.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        Wiki
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'WikiPage',
+}
+</script>
+<style>
+*{
+    padding: 0;
+    margin: 0;
+}
+</style>
